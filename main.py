8e1660318d2525dddc34519ee5f3574bd4645305
yypbd <kyypbd@gmail.com>
2025-01-27 01:48:40 +0900

    fix: main - apply fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import fire
 
 from dotenv import load_dotenv
 
@@ -12,13 +13,44 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 database = Database(DATABASE_URL)
 erd = PlantumlErd(database)
 
-schemas = database.get_schemas()
+# schema = 'public'
+# use_table_comment = True
+# # relation_type = 'none'
+# relation_type = 'laravel'
+#
+# puml = erd.get_erd(schema, use_table_comment, relation_type)
+#
+# print(puml)
 
-schema = 'public'
-use_table_comment = True
-# relation_type = 'none'
-relation_type = 'laravel'
+class FireCommand:
+    def show_schemas(self):
+        return database.get_schemas()
 
-puml = erd.get_erd(schema, use_table_comment, relation_type)
+    def generate_erd(self, schema = '', use_table_comment = False, relation_type = 'none', out_filename = None):
+        if database.get_schemas().count == 0:
+            return 'Schema is empty'
 
-print(puml)
+        if schema == '':
+            schema = database.get_schemas()[0]
+
+        if schema not in database.get_schemas():
+            return 'not exists schemas'
+
+        puml = erd.get_erd(schema, use_table_comment, relation_type)
+        if out_filename is None:
+            return puml
+        else:
+            with open(out_filename, "w") as text_file:
+                text_file.write(puml)
+                text_file.close()
+
+            return "Save puml to " + out_filename
+
+
+if __name__ == '__main__':
+    # fire.Fire(FireCommand)
+
+    puml = FireCommand().generate_erd(schema='public', use_table_comment=False, relation_type='laravel', out_filename='test.puml')
+    print(puml)
+
+    pass
