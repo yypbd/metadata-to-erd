50d7b99fb78592edc1e7e25c255f8012dfeb6115
yypbd <kyypbd@gmail.com>
2025-02-10 02:18:48 +0900

    fix: command - add engine param

--- fire_command.py
+++ fire_command.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
+from src.d2_erd import D2Erd
 from src.database import Database
 from src.plantuml_erd import PlantumlErd
 
@@ -17,7 +18,7 @@ class FireCommand:
         else:
             return "[error] cannot connect to database"
 
-    def generate_erd(self, schema = '', use_table_comment = False, relation_type = 'none', out_filename = None):
+    def generate_erd(self, schema = '', engine = 'puml', use_table_comment = False, relation_type = 'none', out_filename = None):
         if out_filename is not None and os.path.exists(out_filename):
             return '[error] Exists - ' + out_filename
 
@@ -25,7 +26,10 @@ class FireCommand:
         if not database.connect(DATABASE_URL):
             return "[error] cannot connect to database"
 
-        erd = PlantumlErd(database)
+        if engine == 'd2':
+            erd = D2Erd(database)
+        else:
+            erd = PlantumlErd(database)
 
         if database.get_schemas().count == 0:
             return '[error] Schema is empty'
@@ -36,12 +40,12 @@ class FireCommand:
         if schema not in database.get_schemas():
             return '[error] Not exists schemas'
 
-        puml = erd.get_erd(schema, use_table_comment, relation_type)
+        output = erd.get_erd(schema, use_table_comment, relation_type)
         if out_filename is None:
-            return puml
+            return output
         else:
             with open(out_filename, "w") as text_file:
-                text_file.write(puml)
+                text_file.write(output)
                 text_file.close()
 
             return "[success] Save puml to " + out_filename
--- main.py
+++ main.py
@@ -4,14 +4,3 @@ from fire_command import FireCommand
 
 if __name__ == '__main__':
     fire.Fire(FireCommand)
-
-    # print(FireCommand().show_schemas())
-
-    # puml = FireCommand().generate_erd(schema='public', use_table_comment=False, relation_type='laravel', out_filename='test.puml')
-    # print(puml)
-
-    # puml = FireCommand().generate_erd(schema='public', use_table_comment=False, relation_type='laravel')
-    # print(puml)
-
-    # puml = FireCommand().generate_erd(schema='yypbd', use_table_comment=False, relation_type='laravel')
-    # print(puml)
